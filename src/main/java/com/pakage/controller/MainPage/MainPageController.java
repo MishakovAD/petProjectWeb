60a030b18080c412c7485f36915431c3871c9361
Alexandr Mishakov <a.mishakov@i-teco.ru>
2019-05-15 15:33:12 +0300

    Начало работы с Bootstrap. Материал: https://o7planning.org/ru/10749/using-twitter-bootstrap-in-spring-boot#a14709333

--- src/main/java/com/pakage/controller/MainPage/MainPageController.java
+++ src/main/java/com/pakage/controller/MainPage/MainPageController.java
@@ -2,7 +2,10 @@ package com.pakage.controller.MainPage;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Контроллер, обрабатывающий запросы на главной странице.
@@ -11,7 +14,9 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class MainPageController {
     @GetMapping({"/", "/hello"})
-    public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
+    public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name, HttpServletResponse response) {
+        name = "Мир";
+        response.setContentType("text/plain;charset=UTF-8");
         model.addAttribute("name", name);
         return "index";
     }
