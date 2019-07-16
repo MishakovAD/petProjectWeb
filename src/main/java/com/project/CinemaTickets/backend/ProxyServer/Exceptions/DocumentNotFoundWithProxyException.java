597111dbf9290f24cb71e41a81ace7bfc45ecba6
MishakovAD <36512496+MishakovAD@users.noreply.github.com>
2019-07-16 10:19:37 +0300

    Update DocumentNotFoundWithProxyException.java

--- src/main/java/com/project/CinemaTickets/backend/ProxyServer/Exceptions/DocumentNotFoundWithProxyException.java
+++ src/main/java/com/project/CinemaTickets/backend/ProxyServer/Exceptions/DocumentNotFoundWithProxyException.java
@@ -1,5 +1,13 @@
 package com.project.CinemaTickets.backend.ProxyServer.Exceptions;
 
 public class DocumentNotFoundWithProxyException extends Exception {
+    public DocumentNotFoundWithProxyException(String message) {
+    super(message);
+  }
+
+
+  public DocumentNotFoundWithProxyException(String message, Throwable e) {
+    super(message, e);
+  }
 
 }
