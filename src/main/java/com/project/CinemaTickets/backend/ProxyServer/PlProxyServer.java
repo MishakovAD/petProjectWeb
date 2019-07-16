99755ec1c812db2c7419e11514c8aa3c7f1792fe
MishakovAD <36512496+MishakovAD@users.noreply.github.com>
2019-07-16 10:13:27 +0300

    Update PlProxyServer.java

--- src/main/java/com/project/CinemaTickets/backend/ProxyServer/PlProxyServer.java
+++ src/main/java/com/project/CinemaTickets/backend/ProxyServer/PlProxyServer.java
@@ -160,6 +160,7 @@ public class PlProxyServer implements PliProxyServer {
         stringDocument = new StringBuilder();
         BufferedReader reader;
         try {
+            //Add rendom int for different version User-Agent
             connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36");
             connection.setRequestProperty("Referer", "https://www.google.com/");
             connection.connect();
