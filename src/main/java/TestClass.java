3688788f962b4b467ac6c82669c50589f54143ff
Alexandr Mishakov <a.mishakov@i-teco.ru>
2019-09-27 17:05:23 +0300

    Алгоритмы. Лекция Технострим.

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -1,6 +1,29 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class TestClass {
     public static void main(String[] args) throws Exception {
-        System.out.println(fact(170d));
+        //System.out.println(fact(170d));
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        list.forEach(System.out::print);
+        System.out.println();
+
+        System.out.println();
+        int[] arr = new int[] {1, 2, 3, 4, 5, 6, 7, 8};
+        for (int i = 0; i < arr.length / 2; i++) {
+            int current = arr[i];
+            int tmp = arr [arr.length - 1 - i];
+            arr [arr.length - 1 - i] = current;
+            arr[i] = tmp;
+        }
+        Arrays.stream(arr).forEach(System.out::print);
     }
 
     static double fact(double i) {
