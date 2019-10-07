4fae88282eed570d380c7b596d5802eff326d5df
Alexandr.Mishakov <ucoz.1996@mail.ru>
2019-10-07 19:45:02 +0300

    Разбор алгоритма вставками.

---
+++ src/main/java/YandexTest/Exersise1.java
@@ -0,0 +1,38 @@
+package YandexTest;
+
+/*
+Дан лист List<Integer> list = new ArrayList<>();
+
+[1,2,0,0,4,1235125,0,235,0,0,0,25]
+
+Сделать последовательность без нулей за линейной время.
+
+Плохой пример:
+int zeroIndex = 0;
+boolean findZero = false;
+for (int i = 0; i < list.size(); i++) {
+    if (m(list.get(i) && !findZero) {
+        zeroIndex = i;
+        findZero = true;
+    }
+
+    if (!m(list.get(i)) && findZero) {
+        list.set(zeroIndex, list.get(i));
+        zeroIndex++;
+    }
+}
+
+for (int i = zeroIndex; i < list.size(); i++) {
+    list.set(i, 0);
+}
+
+
+private boolean m (Integer num) {
+    int zero = 0;
+    int numI = Integer.toPrimitive(num);
+    return numI == zero;
+}
+ */
+
+public class Exersise1 {
+}
