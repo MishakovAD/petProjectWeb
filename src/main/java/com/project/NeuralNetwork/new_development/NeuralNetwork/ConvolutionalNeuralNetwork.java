092ac0c1c13c72643889ba6d45cc4243f479783e
Alexandr Mishakov <a.mishakov@i-teco.ru>
2019-11-19 18:38:37 +0300

    Начало разработки сверточной нейронной сети

---
+++ src/main/java/com/project/NeuralNetwork/new_development/NeuralNetwork/ConvolutionalNeuralNetwork.java
@@ -0,0 +1,6 @@
+package com.project.NeuralNetwork.new_development.NeuralNetwork;
+
+import com.project.NeuralNetwork.new_development.NeuralNetwork.base.Network;
+
+public class ConvolutionalNeuralNetwork {
+}
---
+++ src/main/java/com/project/NeuralNetwork/new_development/NeuralNetwork/base/CNNetwork.java
@@ -0,0 +1,4 @@
+package com.project.NeuralNetwork.new_development.NeuralNetwork.base;
+
+public class CNNetwork {
+}
